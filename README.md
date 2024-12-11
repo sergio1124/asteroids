324112db0181689b430d28343666b2176d875c79
sergio1124 <mendoza.sergio1124@gmail.com>
2024-12-11 17:35:48 -0600

    Pushing game

---
+++ README.md
@@ -0,0 +1,48 @@
+# Asteroid video game
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Verify Installation](#verify)
+- [](#verify)
+
+## Installation
+
+To get started with this project, you'll need to clone the repository, navigate into the project directory and create a virtual environment at the top level of your project directory and install the pygame dependency. Run the following commands in your terminal:
+
+```bash
+  python3 -m venv venv
+  source venv/bin/activate
+  pip install -r requirements.txt
+```
+
+## Verify
+
+Make sure pygame is installed:
+
+```
+  python3 -m pygame
+```
+
+## Run
+
+To run the game just run the main.py file from your project directory:
+
+```
+  python3 main.py
+```
+
+## Possible Improvements
+
+- [ ] Add a scoring system
+- [ ] Implement multiple lives and respawning
+- [ ] Add an explosion effect for the asteroids
+- [ ] Add acceleration to the player movement
+- [ ] Make the objects wrap around the screen instead of disappearing
+- [ ] Add a background image
+- [ ] Create different weapon types
+- [ ] Make the asteroids lumpy instead of perfectly round
+- [ ] Make the ship have a triangular hit box instead of a circular one
+- [ ] Add a shield power-up
+- [ ] Add a speed power-up
+- [ ] Add bombs that can be dropped
--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	clock = pygame.time.Clock()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroids = pygame.sprite.Group()
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	asteroidField = AsteroidField()
+	shots = pygame.sprite.Group()
+	Shot.containers = (shots, updatable, drawable)
+	dt = 0
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		for obj in updatable:
+			obj.update(dt)
+		for asteroid in asteroids:
+			for shot in shots:
+				if shot.collides_with(asteroid):
+					asteroid.split()
+					shot.kill()
+			if asteroid.collides_with(player):
+				print("Game over!")
+				sys.exit
 		screen.fill("black")
-
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
 
+		# limit the time frame to 60 FPS
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
